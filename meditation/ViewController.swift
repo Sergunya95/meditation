311c48bebcebe39148e1bb43d97ad8eb6eda1e9c
sergunya95 <sergunya95@gmail.com>
2020-12-15 19:35:51 +0700

    Initial Commit

---
+++ meditation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meditation
+//
+//  Created by apple on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
